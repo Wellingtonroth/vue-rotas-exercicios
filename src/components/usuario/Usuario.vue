eb99d1f774a6de8ab90dbaad009416b67cb7428f
Wellington Roth <wellingtonrothh@gmail.com>
2021-02-15 18:17:05 -0300

    usando parametros

--- src/components/usuario/Usuario.vue
+++ src/components/usuario/Usuario.vue
@@ -3,12 +3,18 @@
     <h2>Usuário</h2>
     <hr>
 
+    <p><strong>Código:</strong>{{ id }}</p>
     <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  },
   methods: {
     irParaInicio() {
       // this.$router.push('/')
