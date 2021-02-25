748461312323e475dfe18b6842dd109b6c54167f
Wellington Roth <wellingtonrothh@gmail.com>
2021-02-25 11:13:45 -0300

    router link dinamico

--- src/components/usuario/UsuarioDetalhe.vue
+++ src/components/usuario/UsuarioDetalhe.vue
@@ -2,6 +2,11 @@
   <div class="usuario-detalhe">
     <h3>Usuário Detalhe</h3>
     <p><strong>Código:</strong>{{ id }}</p>
+    
+    <router-link tag="button" primario :to="`/usuario/${id}/editar`">
+    <!-- :to="`/usuario/${$route.params.id}/editar`" -->
+      Editar
+    </router-link>
   </div>
 </template>
 
